9487d58bd2d4e7193623a291bcd3ba6bfd9d7656
doublebottom <30776007+doublebottom@users.noreply.github.com>
2017-10-05 20:19:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from poloniex import PoloniexExtendedAPI
-from helper import *
+import json
+
+def pp(print_me_pretty):
+    """
+    Pretty Print JSON return.
+
+    :param print_me_pretty: Input the return-value of the API queries to just pretty print the JSON return
+    :return:
+    """
+    json_pretty = json.dumps(print_me_pretty, sort_keys=True, indent=2, separators=(',', ': '))
+    return json_pretty
 
 
 def main():
@@ -9,8 +19,8 @@ def main():
     # How to call from core API
     # -------------------------
 
-    # vol = polo.api_query('return24hVolume').json()
-    # print(pp(vol))
+    vol = polo.api_query('return24hVolume').json()
+    print(pp(vol))
 
     # orderbook = polo.api_query('returnOrderBook', params={'currencyPair': 'BTC_BLK', 'depth': '10'}).json()
     # print(pp(orderbook))
